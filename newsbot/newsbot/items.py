77284a3befb5dd7e2cb074b701fc1f56111d7eb3
BoKyeong-Kim <bokyeong3659@gmail.com>
2019-11-13 23:23:29 +0900

    Items file add code

--- newsbot/newsbot/items.py
+++ newsbot/newsbot/items.py
@@ -6,9 +6,13 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.item import Item, Field
 
 
 class NewsbotItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+	company = scrapy.Field()
+    date = scrapy.Field()
+    titles= scrapy.Field()
+    content = scrapy.Field()
+    film_url = scrapy.Field()
+
